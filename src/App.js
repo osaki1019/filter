17b4cb99cfda1477c539fce0bcebb7d98d0565cf
osaki1019 <81526221+osaki1019@users.noreply.github.com>
2022-10-22 22:34:04 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import "./styles.css";
 
 export default function App() {
+  /*
+  filter
+  与えられた配列のうち、条件に合致する要素を取り出し新しい配列を生成する
+  =条件に合致しない要素は削除する
+  */
+  const words = [
+    "spray",
+    "limit",
+    "elite",
+    "exuberant",
+    "destruction",
+    "present"
+  ];
+
+  // 文字の長さが6より小さいものは削除する
+  const result = words.filter((word) => word.length < 6);
+  console.log(result);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>filter関数</h1>
+      {result.map((val) => (
+        <div>{val}</div>
+      ))}
     </div>
   );
 }
